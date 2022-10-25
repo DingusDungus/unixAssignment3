677e8ae441b1d68d2032ac0394727ea48cb17eed
Noah Hakansson <noah.hakansson@protonmail.com>
2022-10-25 22:40:48 +0200

    factorial done with loads of comments

--- CompilerCode/lexyacc-code_lab3/library_functions/src/fact.s
+++ CompilerCode/lexyacc-code_lab3/library_functions/src/fact.s
@@ -1,1 +1,24 @@
- 
+.global fact_func
+   .text
+
+fact_func:
+   pushq %rbp # function enter, push base pointer on to stack
+   movq %rsp, %rbp # set value of base pointer to adress of stack pointer
+   subq $16, %rsp # decrease stack pointer
+   # to make room for local variables
+   movq %rdi, -8(%rbp) # move param value into %rdi
+   cmpq $0, -8(%rbp)
+   jne .factRecurse # recurse unless param is 0, and we return 1.
+   movl $1, %eax # move 1 into %eax and then end function.
+   jmp .factEnd
+.factRecurse:
+   movq -8(%rbp), %rax # save %rax before we deacrease it by 1
+   subq $1, %rax # decrement %rax by 1
+   movq %rax, %rdi # move value into return register %rax
+   call fact_func # recurse
+   imulq -8(%rbp), %rax # when we as caller get back control
+   # multiply and move on to .factEnd to return result
+.factEnd:
+   movq %rbp, %rsp # function leave release stack space
+   popq %rbp # continue from above
+   ret # return control to calling program
--- CompilerCode/lexyacc-code_lab3/library_functions/test.cpp
+++ CompilerCode/lexyacc-code_lab3/library_functions/test.cpp
@@ -2,14 +2,19 @@
 #include <stdint.h>
 
 extern "C" uint64_t inc_func(uint64_t x);
-extern "C" int64_t fact_func(int64_t x);
+extern "C" uint64_t fact_func(uint64_t x);
 extern "C" int64_t lntwo_func(int64_t x);
 extern "C" int64_t gcd_func(int64_t x, int64_t y);
 
 int main(int argc, char **argv) {
+  std::cout << "test inc_func(add_one)" << std::endl;
   for (uint64_t i = 0; i < 10; ++i) {
     std::cout << "iteration: " << i << " inc value: " << inc_func(i)
               << std::endl;
   }
+
+  std::cout << "test fact_func" << std::endl;
+  std::cout << "res: " << fact_func(5) << std::endl;
+
   return 0;
 }
