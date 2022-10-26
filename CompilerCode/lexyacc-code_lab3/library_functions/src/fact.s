8fcee043ea09bc27ee0436e976f6408cd8e8906c
Noah Hakansson <noah.hakansson@protonmail.com>
2022-10-26 22:04:11 +0200

    getting close

--- CompilerCode/lexyacc-code_lab3/library_functions/src/fact.s
+++ CompilerCode/lexyacc-code_lab3/library_functions/src/fact.s
@@ -1,24 +1,28 @@
 .global fact_func
    .text
 
-fact_func:
-   pushq %rbp # function enter, push base pointer on to stack
-   movq %rsp, %rbp # set value of base pointer to adress of stack pointer
-   subq $16, %rsp # decrease stack pointer
-   # to make room for local variables
-   movq %rdi, -8(%rbp) # move param value into %rdi
-   cmpq $0, -8(%rbp)
-   jne .factRecurse # recurse unless param is 0, and we return 1.
-   movl $1, %eax # move 1 into %eax and then end function.
+fact_func: # function enter
+# Prologue
+   # %rbp is the frame pointer, contains snapshot of base pointer
+   pushq %rbp # save current base pointer on to stack
+   movq %rsp, %rbp # set value of base pointer to adress of current stack pointer
+   subq $16, %rsp # reserve 16 bytes for local variables
+# fact
+   movq %rdi, -8(%rbp) # move value at adress -8(%rbp) into %rdi
+   cmpq $0, -8(%rbp) # check if value is 0
+   jne .factCont # jump if value is not 0
+   movl $1, %eax # move 1 into %eax and then jump to end and return
    jmp .factEnd
-.factRecurse:
-   movq -8(%rbp), %rax # save %rax before we deacrease it by 1
+.factCont:
+   movq -8(%rbp), %rax # save %rax at adress -8(%rbp) before we deacrease it by 1
    subq $1, %rax # decrement %rax by 1
    movq %rax, %rdi # move value into return register %rax
-   call fact_func # recurse
-   imulq -8(%rbp), %rax # when we as caller get back control
-   # multiply and move on to .factEnd to return result
-.factEnd:
-   movq %rbp, %rsp # function leave release stack space
-   popq %rbp # continue from above
-   ret # return control to calling program
+   call fact_func # recursive call
+   # when call returns multiply with result from call and result is stored in %rax
+   imulq -8(%rbp), %rax
+   # then move on to .factEnd to return to calling function
+.factEnd: # function leave
+# Epilouge
+   movq %rbp, %rsp # drop stack pointer, release stack space
+   popq %rbp # pop base pointer of stack, so it's restored to before the prolouge
+   ret # return control to calling function
--- CompilerCode/lexyacc-code_lab3/library_functions/test.cpp
+++ CompilerCode/lexyacc-code_lab3/library_functions/test.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+#include <math.h>
 #include <stdint.h>
 
 extern "C" uint64_t inc_func(uint64_t x);
 extern "C" uint64_t fact_func(uint64_t x);
-extern "C" int64_t lntwo_func(int64_t x);
-extern "C" int64_t gcd_func(int64_t x, int64_t y);
+extern "C" uint64_t lntwo_func(uint64_t x);
+extern "C" uint64_t gcd_func(uint64_t x, uint64_t y);
 
 int main(int argc, char **argv) {
-  std::cout << "test inc_func(add_one)" << std::endl;
+  std::cout << "test inc_func(add_one):" << std::endl;
   for (uint64_t i = 0; i < 10; ++i) {
     std::cout << "iteration: " << i << " inc value: " << inc_func(i)
               << std::endl;
   }
 
-  std::cout << "test fact_func" << std::endl;
-  std::cout << "res: " << fact_func(5) << std::endl;
+  std::cout << std::endl << "test fact_func (should be 120):" << std::endl;
+  std::cout << "result: " << fact_func(5) << std::endl;
 
+  std::cout << std::endl << "test lntwo_func (should be 5):" << std::endl;
+  std::cout << "result: " << lntwo_func(32) << std::endl;
+  std::cout << "test lntwo_func (should be 0):" << std::endl;
+  std::cout << "result: " << lntwo_func(1) << std::endl;
+  std::cout << "test lntwo_func (should be 0):" << std::endl;
+  std::cout << "result: " << lntwo_func(0) << std::endl;
   return 0;
 }
